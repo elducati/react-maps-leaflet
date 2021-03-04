8c82406be4059e10b12eb166b058805aeb81135e
elducati <geomondi09@gmail.com>
2021-03-04 11:30:35 +0300

    added current location marker

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import 'leaflet/dist/leaflet.css';
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import LocationMarker from './components/LocationMarker';
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -24,11 +25,7 @@ const App = ()=> {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[-1.285790, 36.820030]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <LocationMarker />
       </MapContainer>
     </div>
   );
---
+++ src/Context.js
@@ -0,0 +1,5 @@
+import {createContext, useContext} from "react"
+
+const Context = createContext(useContext)
+
+export default Context
